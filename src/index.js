68955ffe0e4a9c500b9cf1842e536652009340e1
Matheus Ribeiro Pimenta Nunes <matheuspiment@hotmail.com>
2019-03-10 19:29:49 -0300

    feat: Add initial (root) route
    Close #2

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import nunjucks from 'nunjucks';
 
 const app = express();
 
-nunjucks.configure('views', {
+nunjucks.configure('src/views', {
   autoescape: true,
   express: app,
   watch: true,
 });
 
 app.set('view engine', 'njk');
+
+app.get('/', (_req, res) => res.render('main'));
 
 app.listen(3000);
