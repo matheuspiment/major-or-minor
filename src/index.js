854ecfc892e629955d3700f0c3fc57e72fc19559
Matheus Ribeiro Pimenta Nunes <matheuspiment@hotmail.com>
2019-03-10 19:54:03 -0300

    feat: Add `/check` route
    Close #3

--- src/index.js
+++ src/index.js
@@ -9,8 +9,19 @@ nunjucks.configure('src/views', {
   watch: true,
 });
 
+app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'njk');
 
 app.get('/', (_req, res) => res.render('main'));
+
+app.post('/check', (req, res) => {
+  const { age } = req.body;
+
+  if (age > 18) {
+    res.redirect(`/major?age=${age}`);
+  }
+
+  res.redirect(`/minor?age=${age}`);
+});
 
 app.listen(3000);
